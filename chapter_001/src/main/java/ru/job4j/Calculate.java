9fc535ccd47df74a80053b1645105316e165a94e
Gregory Petrukhin <parentalbox@gmail.com>
2018-05-16 19:05:05 +0300

    1.5. Checkstyle[#53487]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -3,13 +3,13 @@ package ru.job4j;
 	/**
 	* Example class for study tasks.
 	*/
-public class Calculate{
+public class Calculate {
 	/**
 	* Entering point.
 	* Prints "Hello World!!" to console out.
 	* @param args - args.
 	*/
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 	}
 	
