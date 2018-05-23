15aea71fadee337ef3f810e5505db930750a40a5
Gregory Petrukhin <parentalbox@gmail.com>
2018-05-23 15:49:33 +0300

    4.2. Создать программу вычисляющую факториал.[#53969] refactored calc(n) method to use loop instead of recursion.

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -10,6 +10,17 @@ public class Factorial {
     public int calc(int n) {
         if (n < 0) throw new IllegalArgumentException();
         else if (n == 0 || n == 1) return 1;
-        return n * calc(n - 1);
+        int result = 1;
+        for (int i = 1; i < n + 1; i++) {
+            result = result * i;
+        }
+        return result;
     }
+
+    /*  this is beutiful solution
+        public int calc(int n) {
+        if (n < 0) throw new IllegalArgumentException();
+        else if (n == 0 || n == 1) return 1;
+        return n * calc(n - 1);
+    }*/
 }
