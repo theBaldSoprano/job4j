307b2264403557831328d28dd150b660945f00e2
Gregory Petrukhin <parentalbox@gmail.com>
2018-05-28 17:51:13 +0300

    5.7. Квадратный массив заполнен true или false по диагоналям.[#54778]  убрал множественный return

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -2,22 +2,16 @@ package ru.job4j.array;
 
 public class MatrixCheck {
     public boolean mono(boolean[][] data) {
-        boolean result = true;
+        boolean diag1 = true;
+        boolean diag2 = true;
         for (int i = 0; i < data.length - 1; i++) {
             if (data[i][i] != data[i + 1][i + 1]) {
-                result = false;
-                break;
+                diag1 = false;
+            }
+            if (data[i][data.length - i - 1] != data[i + 1][data.length - i - 2]){
+                diag2 = false;
             }
         }
-        if (result) {
-            return result;
-        }
-        result = true;
-        for (int i = 0; i < data.length - 1; i++) {
-            if (data[i][data.length - i - 1] != data[i + 1][data.length - i - 2]) {
-                return false;
-            }
-        }
-        return result;
+        return diag1 || diag2;
     }
 }
