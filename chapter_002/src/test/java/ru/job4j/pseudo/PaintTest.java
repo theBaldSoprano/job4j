f60d19e324054af516758d25c996051f4da0b6d5
Gregory Petrukhin <parentalbox@gmail.com>
2018-06-06 17:18:28 +0300

    5. Рефакторинг теста @Before @After[#56327]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,17 +1,31 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+    PrintStream stdout = System.out;
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void setOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void setStdOutput() {
+        System.setOut(stdout);
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
                 new String(out.toByteArray()),
@@ -28,6 +42,25 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("   /\\")
+                                .append(System.lineSeparator())
+                                .append("  /  \\")
+                                .append(System.lineSeparator())
+                                .append(" /    \\")
+                                .append(System.lineSeparator())
+                                .append("/______\\")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
     }
 }
